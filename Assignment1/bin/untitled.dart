2a71e061ad4ab867a4fdb2f9502294eb2c28e7e3
Sheikh Mohammad Shaqib <smshaqib@gmail.com>
2024-03-10 16:36:33 +0600

    a

--- Assignment1/bin/untitled.dart
+++ Assignment1/bin/untitled.dart
@@ -1,4 +1,30 @@
 void main(){
 
-  print("hello");
+  Media m = new Media();
+  Song s = new Song("Avijit");
+
+  m.play();
+  s.play();
+
+}
+
+class Media{
+
+  void play(){
+    print("Playing media...");
+  }
+}
+
+class Song extends Media{
+
+  String artist='';
+
+  Song (String artist) {
+    this.artist = artist;
+  }
+
+  void play(){
+    print("Playing song by $artist...");
+  }
+  
 }
