709537a91ef5f4ac7ccdae4051848f452724233d
Sheikh Mohammad Shaqib <smshaqib@gmail.com>
2024-05-14 23:25:18 +0600

    added

---
+++ assignment3/lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(
+
+        // title: Text('shaqib'),
+      ),
+
+
+      body: Column(
+
+
+        children: [
+
+
+
+          Container(
+
+            height: 100,
+            width: 340,
+            color: Colors.green,
+
+
+
+          ),
+
+          Container(),
+
+          Container(),
+
+        ],
+
+
+      ),
+
+
+    );
+  }
+}
